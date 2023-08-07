3c1b292948e9a998cb7daebfb7c8c44ba2698dc4
Sharan <sharan@mallow-tech.com>
2023-08-07 12:06:21 +0530

    - Removed unwanted lines for functionality

--- system/docker/Exec.py
+++ system/docker/Exec.py
@@ -4,5 +4,5 @@ class Exec:
     def __init__(self) -> None:
         pass
 
-    def run(self, containerName: str, command: str):
-        return os.system('docker exec -it ' + containerName + ' ' + command)
+    def run(self, container: str, command: str):
+        return os.system('docker exec -it ' + container + ' ' + command)
