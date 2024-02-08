a5a12d77a1bc5c0046327c2def72241042b810be
Sharan <sharan@mallow-tech.com>
2024-02-08 12:24:25 +0530

    - Added Options for Additional Hosts

--- system/docker/App.py
+++ system/docker/App.py
@@ -24,7 +24,7 @@ class App:
 
         return "HostRegexp(`" + ".".join(hosts) + "`)"
 
-    def __get_proxy_labels(self, host: str, container_port: int = 80) -> dict:
+    def __generate_proxy_labels_for_host(self, host: str, container_port: int = 80) -> dict:
         host_key = self.__app.get_project_key(host)
         secure_key = host_key + "_secure"
         proxy_rule = self.__get_proxy_rule(host)
@@ -40,6 +40,14 @@ class App:
             "traefik.http.services." + secure_key + "_service.loadbalancer.server.port": str(container_port),
         }
 
+    def __get_proxy_labels(self, host: str, container_port: int = 80, additional_hosts: list = []) -> dict:
+        proxy_labels = self.__generate_proxy_labels_for_host(host, container_port)
+
+        for additional_host in additional_hosts:
+            proxy_labels.update(self.__generate_proxy_labels_for_host(additional_host, container_port))
+
+        return proxy_labels
+
     def __get_app_container_labels(self, host: str) -> dict:
         return {
             "com.example.vendor": self.__app.name,
@@ -48,27 +56,40 @@ class App:
             "com.example.service": "Application",
         }
 
-    def __prepare_labels(self, host: str, labels: dict = {}, container_port: int = 80) -> dict:
-        labels.update(self.__get_proxy_labels(host, container_port))
+    def __prepare_labels(self, host: str, labels: dict = {}, container_port: int = 80, additional_hosts: list = []) -> dict:
+        labels.update(self.__get_proxy_labels(host, container_port, additional_hosts))
         labels.update(self.__get_app_container_labels(host))
 
         return labels
 
+    def __process_additional_hosts(self, additional_hosts: {list, None}):
+        if additional_hosts is None:
+            return []
+
+        processed_hosts = []
+        for host in additional_hosts.split(','):
+            processed_hosts.append(host.strip())
+
+        return processed_hosts
+
     def run(
-        self,
-        host: str,
-        image: str,
-        labels: dict = {},
-        envs: dict = {},
-        container_port: int = 80,
-        volumes: list = [],
-        ports: dict = {},
+            self,
+            host: str,
+            image: str,
+            labels: dict = {},
+            envs: dict = {},
+            container_port: int = 80,
+            volumes: list = [],
+            ports: dict = {},
+            additional_hosts: list = []
     ) -> None:
+        additional_hosts = self.__process_additional_hosts(additional_hosts)
+
         self.__container.run(
             image=image,
             name=self.__app.get_container_name(host),
             volumes=volumes,
-            labels=self.__prepare_labels(host, labels, container_port),
+            labels=self.__prepare_labels(host, labels, container_port, additional_hosts),
             environment=envs,
             ports=ports,
         )
